d1cb6ac65fd34a3224032bd1f1bd4e7027435f16
전상문맥북 <smdeveloperman@gmail.com>
2025-04-19 21:51:16 +0900

    디렉토리구조 변경

---
+++ 전상문/practice/Solution.java
@@ -0,0 +1,58 @@
+package practice;
+
+class Solution {
+    public int solution(int n, int w, int num) {
+        int answer = 1;
+        
+        /* 전체 택배 상자 셋팅 */
+        int row = n / w;
+        // 나머지가 있을경우 한줄 추가
+        if (n % w != 0 ){
+            row += 1;
+        } 
+
+        /* 입력된 상자 처리 */
+
+        int first_row = num / w ; // 첫번째 행
+        int first_column = num % w; // 첫번째 열
+        
+        int curr_num = num;
+
+        // 나머지가 없는경우 마지막 수를 의미
+        if (first_column != 0){
+            first_row += 1;
+        }else{
+            // 나머지가 0이면 각 행의 w번재 수
+            first_column = w;
+        }
+
+        //짝수일 경우 
+        if (first_row % 2 == 0){
+            first_column = w - first_column + 1;
+        }
+
+
+        for(int i = first_row; i < row ; i++){
+            //현재 줄이 홀수일경우 왼 -> 오른쪽으로 숫자 증가
+            if(i % 2 != 0){
+                curr_num = (w - first_column) * 2 + curr_num + 1;
+            }else{
+                curr_num = (first_column-1) * 2 + curr_num + 1;
+            }
+            // 현재 추적적인 박스 숫자가 전체 n개를 넘을경우 
+            if (curr_num > n){
+                break;
+            }
+            answer+=1;
+        }
+
+        
+        return answer;
+
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int result = s.solution(22, 8, 3); // 테스트 값
+        System.out.println("결과: " + result);
+    }
+}
--- 전상문/practice/Solution2.java
+++ 전상문/practice/Solution2.java
@@ -1,16 +1,24 @@
+package practice;
 import java.util.Scanner;
 
-public class Solution {
+public class Solution2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         int answer = 0;
         
-        for(int i=0; number>0; i++){
+        System.out.println(String.valueOf(number).length()/2);
+        
+        for(int i = 0; number > 0; i++){
+            System.out.println("==========");
+            System.out.println(number);
             answer += number % 100;
             number /= 100;
         }
-
+        System.out.println("정답");
         System.out.println(answer);
     }
 }
+
+
+
