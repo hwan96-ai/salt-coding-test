b4895ff8c9d928fb5a54ccb6f1d94804b53d2862
smdeveloper7 <162524295+smdeveloper7@users.noreply.github.com>
2025-04-20 00:58:19 +0900

    [level 0] Title: 문자열로 변환, Time: 1.17 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 전상문/프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int n) {
+        
+        String answer = String.format("%d", n);
+
+        //이스케이프(escape)는 특수 문자나 예약 문자를 출력하거나 제어하기 위해 사용하는 방식
+        
+        return answer;
+    }
+}
