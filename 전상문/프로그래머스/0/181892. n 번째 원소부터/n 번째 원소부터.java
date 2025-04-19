6432c4f10666df668e37f468e9fa204a40978bf6
smdeveloper7 <162524295+smdeveloper7@users.noreply.github.com>
2025-04-20 01:16:26 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 87.5 MB -BaekjoonHub

--- 전상문/프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 전상문/프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int n_idx = n - 1;
+        int n_idx = n - 1; //인덱스는 0부터 시작하므로 
         int[] answer = new int[num_list.length - n_idx]; //
         for(int i = 0; i < num_list.length - n_idx ; i++){
-            answer[i] = num_list[n_idx+i]; //인덱스는 0부터 시작하므로 
+            answer[i] = num_list[n_idx+i];
         }
         return answer;
     }
